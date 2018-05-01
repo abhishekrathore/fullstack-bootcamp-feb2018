2feaddb61a723e95bbd30df1192641391b39822d
Abhishek Rathore <rathore.abhishek87@gmail.com>
2018-05-01 16:05:59 +0530

    connecting database and sever apis

---
+++ Day21/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+var server = express();
+
+var url = "mongodb://localhost:27017"
+
+var DB;
+
+MongoClient.connect(url, function(err, client) {
+    if(err==null){
+       DB = client.db("nodeClass");
+       console.log("connected to DB")
+    }else{
+        console.log(err);
+    }
+})
+
+server.get("/data",function(req,res){
+        var name =  req.query.playerName;
+    DB.collection("players").find({"name":name}).toArray(function(err,result){
+       res.json(result)
+    })
+
+
+})
+
+
+server.listen(8000,function(){
+    console.log("server started")
+})
